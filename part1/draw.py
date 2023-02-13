ef06cae6586c16fbd27f89cb27df24f48dbe196e
Alexander Rupp-Coppi <amr2311@columbia.edu>
2023-02-13 10:22:08 -0500

    reading from webcam, superimposing text annotation

---
+++ part1/draw.py
@@ -0,0 +1,9 @@
+import cv2 as cv
+import numpy as np
+
+blank = np.zeros((500,500,3), dtype='uint8')
+
+img = cv.imread('photos/cat.jpg')
+
+
+
--- part1/read.py
+++ part1/read.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import time
 
 # img = cv.imread('photos/cat.jpg')
 # cv.imshow('Cat', img)
@@ -7,16 +8,22 @@ import cv2 as cv
 
 capture = cv.VideoCapture(0)
 
+font = cv.FONT_HERSHEY_TRIPLEX
+font_color = (255, 125, 255)
+font_thickness = 2
+
 while True: 
     isTrue, frame = capture.read()
 
     if isTrue:
+        coords = (int(frame.shape[1] * 0.2), int(frame.shape[0] * 0.75))
+
+        cv.putText(frame, 'Hello, world', coords, font, 1.0, font_color, font_thickness)
+
         cv.imshow('Video', frame)
 
-    if 0xFF==ord('d'): 
+    if cv.waitKey(0) & 0xFF == ord('q'): 
         break
-
-    cv.waitKey(0)
 
 capture.release()
 cv.destroyAllWindows()
